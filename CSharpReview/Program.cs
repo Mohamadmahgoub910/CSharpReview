1540252ddb7f8e33e69392e8d11bfdef5d1a2454
Mohamadmahgoub910 <mohamadmahgoub1@Outlook.com>
2025-10-18 23:37:56 +0300

    Add project files.

---
+++ CSharpReview/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace CSharpReview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            // main should br static { startup point of the program}
+            // Console is a class in System namespace
+            // CLR (Common Language Runtime) executes the program
+            // CTS (Common Type System) Value Types and Reference Types
+            // Value Types : int, float, double, bool, char, struct, enum
+            // Reference Types : string, class, interface, array, delegate
+        }
+    }
+}
